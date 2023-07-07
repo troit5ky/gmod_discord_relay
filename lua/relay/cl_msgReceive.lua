ccbe7cd4bad31c979fd2a4bb51b2a27b19824726
Dmitry Troitsky <testeron211@gmail.com>
2023-07-08 01:19:04 +0300

    cache chat.AddText is buggy

--- lua/relay/cl_msgReceive.lua
+++ lua/relay/cl_msgReceive.lua
@@ -1,7 +1,5 @@
-local chat_AddText = chat.AddText
-
 net.Receive("!!discord-receive", function()
 	local msg = net.ReadTable()
 
-	chat_AddText( Discord.prefixClr, "["..Discord.prefix.."] ", Color(255, 255, 255), msg.author..": ", msg.content )
+	chat.AddText( Discord.prefixClr, "["..Discord.prefix.."] ", Color(255, 255, 255), msg.author..": ", msg.content )
 end)
